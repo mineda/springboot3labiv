b72fd8151f5be6f777fedc6547162b0ca3d8a15c
Emanuel Mineda Carneiro <mineda@gmail.com>
2023-08-15 20:01:58 -0300

    Commit inicial

---
+++ src/main/java/br/gov/sp/fatec/springboot3labiv/Springboot3labivApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springboot3labiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3labivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3labivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/springboot3labiv/Springboot3labivApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springboot3labiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3labivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
