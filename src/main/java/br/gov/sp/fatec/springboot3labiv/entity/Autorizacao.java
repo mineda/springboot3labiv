dba6ed20901e92c1d17cacdef923452fcf709fd2
Emanuel Mineda Carneiro <mineda@gmail.com>
2023-08-24 01:39:09 +0000

    Relacionamentos N-N e 1-N

---
+++ src/main/java/br/gov/sp/fatec/springboot3labiv/entity/Autorizacao.java
@@ -0,0 +1,62 @@
+package br.gov.sp.fatec.springboot3labiv.entity;
+
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "aut_autorizacao")
+public class Autorizacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "aut_id")
+    private Long id;
+
+    @Column(name = "aut_nome")
+    private String nome;
+    
+    @JsonIgnore
+    @ManyToMany(mappedBy = "autorizacoes", fetch = FetchType.LAZY)
+    private Set<Usuario> usuarios;
+
+    public Autorizacao(String nome) {
+        this.nome = nome;
+    }
+
+    public Autorizacao() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(Set<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+}
--- src/main/java/br/gov/sp/fatec/springboot3labiv/entity/Usuario.java
+++ src/main/java/br/gov/sp/fatec/springboot3labiv/entity/Usuario.java
@@ -1,10 +1,18 @@
 package br.gov.sp.fatec.springboot3labiv.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,12 +30,24 @@ public class Usuario {
     @Column(name = "usr_senha")
     private String senha;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "uau_usuario_autorizacao",
+        joinColumns = {@JoinColumn(name = "usr_id")},
+        inverseJoinColumns = {@JoinColumn(name = "aut_id")})
+    private Set<Autorizacao> autorizacoes;
+
+    @OneToMany(mappedBy = "usuario")
+    private Set<Anotacao> anotacoes;
+
     public Usuario(String nome, String senha) {
+        this();
         this.nome = nome;
         this.senha = senha;
     }
 
-    public Usuario() {}
+    public Usuario() {
+        this.autorizacoes = new HashSet<Autorizacao>();
+    }
 
     public Long getId() {
         return id;
@@ -51,6 +71,22 @@ public class Usuario {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public Set<Autorizacao> getAutorizacoes() {
+        return autorizacoes;
+    }
+
+    public void setAutorizacoes(Set<Autorizacao> autorizacoes) {
+        this.autorizacoes = autorizacoes;
+    }
+
+    public Set<Anotacao> getAnotacoes() {
+        return anotacoes;
+    }
+
+    public void setAnotacoes(Set<Anotacao> anotacoes) {
+        this.anotacoes = anotacoes;
     }
     
 }
